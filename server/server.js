3bba2490178e204cedfbbaa511b191676c57daba
IT-YASH <pyash7646@gmail.com>
2025-11-03 17:29:15 +0530

    updated the server.js file

--- server/server.js
+++ server/server.js
@@ -25,5 +25,3 @@ if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => console.log("Server is running on port", PORT));
 }
-
-export default app;
