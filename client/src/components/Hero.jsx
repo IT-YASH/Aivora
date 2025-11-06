5cf928310d9d3b61c785e256a3ddef9c211519d7
IT-YASH <pyash7646@gmail.com>
2025-11-06 16:25:36 +0530

    updated the final code

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -27,13 +27,6 @@ const Hero = () => {
         >
           Start Creating Now
         </button>
-        <button className="bg-white px-10 py-3 rounded-lg border border-gray-300 hover:scale-102 active:scale-95 transition cursor-pointer">
-          Watch Demo
-        </button>
-      </div>
-      <div className="flex item-center gap-4 mt-8 mx-auto text-gray-600">
-        <img src={assets.user_group} className="h-8" alt="" />
-        Trusted by 10k+ People
       </div>
     </div>
   );
